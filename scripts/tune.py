088e5601e322f72b0727e46036bf1e98ed89c558
KevinHOARAU974 <kevin.hoarau@ens-paris-saclay.fr>
2026-04-15 17:15:17 +0200

    Add Optuna pipeline in tune.py and clean train.py

--- scripts/tune.py
+++ scripts/tune.py
@@ -1,3 +1,4 @@
+import optuna
 import aegnn
 import datetime
 import lightning.pytorch as pl
@@ -5,48 +6,38 @@ import torch
 import argparse
 import yaml
 import os
+import copy
 
 from pathlib import Path
+from functools import partial
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping, LearningRateMonitor, TQDMProgressBar
+from optuna.integration import PyTorchLightningPruningCallback
+
 
 def load_config(config_path: str) -> dict:
     with open(config_path, "r", encoding="utf-8") as f:
         return yaml.safe_load(f)
     
-def main() -> None:
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--config",
-        type=str,
-        required=True,
-        help="Path to YAML config file",
-    )
-    args = parser.parse_args()
-
-    cfg = load_config(args.config)
-
-    print(cfg)
-
-    os.environ["AEGNN_DATA_DIR"] = cfg["data_dir"]
-    print(os.environ["AEGNN_DATA_DIR"])
-
-    pl.seed_everything(cfg["seed"], workers=True)
-
-    training(cfg)
-
-
-def training(cfg):
+def objective(trial: optuna.Trial, base_cfg):
+    
 
     gpu = True if torch.cuda.is_available() else False
 
+    cfg = copy.deepcopy(base_cfg)
+
+    #Optuna
+    cfg["model_params"]["lr"] = trial.suggest_float("lr", 1e-4, 5e-3, log=True)
+    cfg["model_params"]["weight_decay"] = trial.suggest_float("weight_decay", 1e-6, 1e-3, log=True)
+
+    pl.seed_everything(cfg["seed"], workers=True)
+
+    print(cfg)
+
     #Load data module
     data_module = aegnn.datasets.by_name(cfg['dataset']).from_cfg(cfg['data_params'])
-    # print('module créé')
     data_module.setup()
 
-    #Create model
     model = aegnn.models.by_task(cfg['task'])(cfg["model"],
                                       cfg["dataset"],
                                       num_classes=data_module.num_classes,
@@ -55,32 +46,11 @@ def training(cfg):
                                       bias = True,
                                       root_weight = True, 
                                       **cfg["model_params"])
-    # print("model créé")
-
-    project = f"aegnn-{cfg['dataset']}-{cfg['task']}"
-    experiment_name = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-
-
-    #Loggers
-    wandb_logger = WandbLogger(project="aegnn",
-                        #    groupe = "ncars",
-                           name=f"{project}_{experiment_name}")
     
-    wandb_logger.experiment.config.update(cfg)
-    wandb_logger.experiment.config.update(aegnn.utils.git.get_git_info())
-    wandb_logger.experiment.config.update({"git_dirty":aegnn.utils.git.is_dirty()})
-
-    print(aegnn.utils.git.get_git_info())
-
-    wandb_logger.experiment.define_metric("epoch")
-    wandb_logger.experiment.define_metric("*", step_metric="epoch")
-
-    loggers = []
-    loggers.append(wandb_logger)
+    experiment_name = f"trial_{trial.number}"
 
     checkpoint_path = os.path.expanduser(os.path.join(cfg["log_dir"], "checkpoints", cfg["dataset"], cfg["task"], experiment_name))
     Path(checkpoint_path).mkdir(parents=True,exist_ok=True)
-
 
     #Callbacks
 
@@ -91,45 +61,82 @@ def training(cfg):
         monitor="Val/loss",
         mode='min',
         save_top_k=1,
-        save_last=True,
+        save_last=False,
         auto_insert_metric_name=False
     )
 
     #Early Stopping
     early_stopping = EarlyStopping(
         monitor="Val/loss",
-        patience=10,
+        patience=5,
         mode='min',
         verbose=True
     )
 
-    lr_monitor = LearningRateMonitor(logging_interval="epoch")
-
-    progress_bar =TQDMProgressBar(refresh_rate=1)
+    #Optuna Pruning Callback
+    pruning = PyTorchLightningPruningCallback(trial, monitor="Val/Accuracy")
 
     callbacks = [
         checkpoint_callback,
         early_stopping,
-        lr_monitor,
-        progress_bar
+        pruning
     ]
-
-    #Training
 
     trainer_kwargs = {
     "accelerator":"gpu" if gpu else "cpu",
     "profiler": "simple" if cfg["profile"] else False,
     }
 
-    trainer = pl.Trainer(logger=loggers,
+    trainer = pl.Trainer(logger=False,
                      callbacks=callbacks,
                      **cfg["trainer"],
                      **trainer_kwargs
                      )
     
-    print("trainer créé")
-
     trainer.fit(model, datamodule=data_module)
 
+    metric = trainer.callback_metrics.get("Val/Accuracy")
+    if metric is None:
+        raise RuntimeError("Val/Accuracy not find")
+    
+    return float(metric.cpu().item())
+
 if __name__ == "__main__":
-    main()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="Path to YAML config file",
+    )
+    args = parser.parse_args()
+
+    base_cfg = load_config(args.config)
+
+    os.environ["AEGNN_DATA_DIR"] = base_cfg["data_dir"]
+    print(os.environ["AEGNN_DATA_DIR"])
+
+    objective_fn = partial(objective, base_cfg=base_cfg)
+
+    study = optuna.create_study(direction="maximize")
+    study.optimize(objective_fn, n_trials=1)
+
+    print("Best trial:")
+    print(study.best_trial.number)
+    print(study.best_trial.value)
+    print(study.best_trial.params)
+
+    cfg = copy.deepcopy(base_cfg)
+    cfg["model_params"]["lr"] = study.best_trial.params["lr"]
+    cfg["model_params"]["weight_decay"] = study.best_trial.params["weight_decay"]
+
+    project = f"aegnn-{cfg['dataset']}-{cfg['task']}-optuna"
+
+    wandb_logger = WandbLogger(project="aegnn",
+                        #    groupe = "ncars",
+                           name=f"{project}_best_trial")
+    
+    wandb_logger.experiment.config.update(cfg)
+    wandb_logger.experiment.config.update(aegnn.utils.git.get_git_info())
+    wandb_logger.experiment.config.update({"git_dirty":aegnn.utils.git.is_dirty()})
