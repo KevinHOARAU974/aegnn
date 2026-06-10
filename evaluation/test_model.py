f945b4c96270e92ebe862e3a3c49c63720b1a500
KevinHOARAU974 <kevin.hoarau@ens-paris-saclay.fr>
2026-06-10 13:49:29 +0200

    Update test_model.py

--- evaluation/test_model.py
+++ evaluation/test_model.py
@@ -6,6 +6,8 @@ import yaml
 import argparse
 import lightning.pytorch as pl
 
+from pathlib import Path
+
 
 def load_config(config_path: str) -> dict:
     with open(config_path, "r", encoding="utf-8") as f:
@@ -16,18 +18,30 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--path",
+        "--folder_path",
         type=str,
         required=True,
         help="Path to folder",
     )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        required=True,
+        help="Path to dat",
+    )
+    parser.add_argument(
+        "--csv_path",
+        type=str,
+        required=True,
+        help="Path to csv file",
+    )
     args = parser.parse_args()
 
-    cfg = load_config(f'{args.path}/config.yaml')
+    cfg = load_config(f'{args.folder_path}/config.yaml')
 
     print(cfg)
 
-    os.environ["AEGNN_DATA_DIR"] = cfg["data_dir"]['value']
+    os.environ["AEGNN_DATA_DIR"] = args.data_path #cfg["data_dir"]['value']
     print(os.environ["AEGNN_DATA_DIR"])
 
     pl.seed_everything(cfg["seed"]['value'], workers=True)
@@ -38,18 +52,18 @@ def main():
     # print('module créé')
     data_module.setup()
 
-    model_path = f"{args.path}/best.ckpt"
+    model_path = f"{args.folder_path}/best.ckpt"
 
     # Load the model
     model = aegnn.models.recognition.RecognitionModel.load_from_checkpoint(model_path, 
-                                      cfg["model"]['value'],
-                                      cfg["dataset"]['value'],
+                                      network=cfg["model"]['value'],
+                                      dataset=cfg["dataset"]['value'],
                                       num_classes=data_module.num_classes,
                                       img_shape=data_module.dims,
                                       max_epochs = cfg["trainer"]['value']["max_epochs"],
                                       bias = True,
                                       root_weight = True,
-                                      log_dir = args.path,
+                                      log_dir = args.folder_path,
                                       **cfg["model_params"]['value'])
     # print("model créé")
 
@@ -58,8 +72,32 @@ def main():
     ##### Accuracy + Confusion matrix
     test_loader = data_module.test_dataloader()
 
-    results = trainer.test(model, dataloaders=test_loader)
+    results_test = trainer.test(model, dataloaders=test_loader)[0]
 
     ##### Number of parameters
 
     num_params = sum(p.numel() for p in model.parameters())
+
+    ### Save in csv files
+
+    file_exists = Path(args.csv_path).exists()
+
+    results = {
+        'model_name' : cfg["model"]['value'],
+        'dataset': cfg["dataset"]['value'],
+        'seed': cfg["seed"]['value'],
+        'test_loss': results_test["Test/loss"],
+        'test_acc' : results_test["Test/Accuracy"],
+        'num_params': num_params
+    }
+
+    print(results)
+
+    df = pd.DataFrame([results])
+
+    df.to_csv(args.csv_path, mode="a", header=not file_exists, index=False)
+
+if __name__ == "__main__":
+    main()
+
+    
